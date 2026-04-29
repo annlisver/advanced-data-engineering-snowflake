fe486ab05fc483679901489ddd3ea6a6843362aa
Anna Veremchuk <20599050+annlisver@users.noreply.github.com>
2026-04-28 23:09:02 -0400

    changes

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -64,8 +64,7 @@ CREATE TABLE {{env}}_tasty_bytes.raw_pos.country
    iso_currency VARCHAR(3),
    iso_country VARCHAR(2),
    city VARCHAR(16777216),
-   city_population VARCHAR(16777216),
-   city_id NUMBER(19,0)
+   city_population VARCHAR(16777216)
 );
 
 
@@ -294,8 +293,8 @@ USE WAREHOUSE demo_build_wh;
 
 
 -- country table load
-COPY INTO {{env}}_tasty_bytes.raw_pos.country
-FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
+--COPY INTO {{env}}_tasty_bytes.raw_pos.country
+--FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
 
 
 -- franchise table load
